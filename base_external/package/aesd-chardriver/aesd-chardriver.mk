5e8ed6025576a57ddf8c4ea03cfadb52eca9931e
rcmb <rajeshkc@live.ca>
2023-11-14 03:41:20 +0000

    Driver and Overlay updates

--- base_external/package/aesd-chardriver/aesd-chardriver.mk
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -5,13 +5,13 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION =  6543d1538468c3d8877f3ad15686912ce6481f97
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-rcmb.git
+AESD_CHARDRIVER_VERSION =  6543d1538468c3d8877f3ad15686912ce6481f97
+AESD_CHARDRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-rcmb.git
 
 AESD_CHARDRIVER_SITE_METHOD = git
 AESD_CHARDRIVER_GIT_SUBMODULES = YES
 
-AESD_CHARDRIVER_MODULE_SUBDIRS = aesd-char-driver/
+AESD_CHARDRIVER_MODULE_SUBDIRS += aesd-char-driver/
 
 #defining a set of installation commands for the build system
 define AESD_CHARDRIVER_INSTALL_TARGET_CMDS
---
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+MODULE_LOAD_EXEC=/sbin/modprobe
+MODULE_UNLOAD_EXEC=/sbin/rmmod
+
+# Check if the executables exist
+if [ ! -x "$MODULE_LOAD_EXEC" ] || [ ! -x "$MODULE_UNLOAD_EXEC" ]; then
+    echo "Module-related executables not found at $MODULE_LOAD_EXEC or $MODULE_UNLOAD_EXEC"
+    exit 1
+fi
+
+# Function to start the modules
+start_modules() {
+    echo "Loading scull and faulty modules"
+    $MODULE_LOAD_EXEC faulty
+    #scull_load
+    /usr/bin/aesdchar_load   
+    $MODULE_LOAD_EXEC hello
+}
+
+# Function to stop the modules
+stop_modules() {
+    echo "Unloading scull and faulty modules"
+    $MODULE_UNLOAD_EXEC faulty
+    #scull_unload
+    /usr/bin/aesdchar_unload  
+    $MODULE_UNLOAD_EXEC hello
+}
+
+# Check for the start or stop command
+case "$1" in
+    start)
+        start_modules
+        ;;
+    stop)
+        stop_modules
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
+
+
+
